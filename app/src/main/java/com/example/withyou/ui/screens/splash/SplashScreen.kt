417d703d2d1e5ddce82d819f78d4cb4cb15f6308
Amayl97 <codeamayl97@gmail.com>
2026-08-10 11:20:37 +0500

    Created Splash Screen

--- app/src/main/java/com/example/withyou/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/withyou/ui/screens/splash/SplashScreen.kt
@@ -1,8 +1,19 @@
 package com.example.withyou.ui.screens.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.withyou.R
 import com.example.withyou.authentication.presentation.AuthViewModel
 import kotlinx.coroutines.delay
 
@@ -22,5 +33,23 @@ fun SplashScreen(
             onNavigateToLogin()
         }
     }
-    Text("Splash Screen")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+        ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "Withyou Logo",
+                modifier = Modifier.size(160.dp)
+            )
+            Text(
+                text = "Withyou",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
 }
