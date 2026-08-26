1e61433bc0b44c15113c3eb04382e9aae1a3028c
Amayl97 <codeamayl97@gmail.com>
2026-08-26 13:48:57 +0500

    Clean up video upload code

--- app/src/main/java/com/example/withyou/ui/screens/upload/UploadViewModel.kt
+++ app/src/main/java/com/example/withyou/ui/screens/upload/UploadViewModel.kt
@@ -16,7 +16,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.util.UUID
+
 
 @HiltViewModel
 class UploadViewModel @Inject constructor(
