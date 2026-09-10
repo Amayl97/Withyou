ee959eeaf02b3aad3be735961450c8995f2c2dcc
Amayl97 <codeamayl97@gmail.com>
2026-09-10 09:04:10 +0500

    feat: initialize RevenueCat SDK

--- app/src/main/java/com/example/withyou/WithYouApplication.kt
+++ app/src/main/java/com/example/withyou/WithYouApplication.kt
@@ -1,7 +1,21 @@
 package com.example.withyou
 
 import android.app.Application
+import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WithYouApplication : Application()
+class WithYouApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Purchases.configure(
+            PurchasesConfiguration.Builder(
+                this,
+                BuildConfig.REVENUECAT_TEST_API_KEY
+            ).build()
+        )
+    }
+}
